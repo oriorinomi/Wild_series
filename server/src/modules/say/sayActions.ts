f59dba5a234a7116886de2670f6016035fac2d2b
AURELIA GUARINO <aureliaguarino0@gmail.com>
2024-12-09 16:38:55 +0100

    [Feat] create category

--- server/src/modules/say/sayActions.ts
+++ server/src/modules/say/sayActions.ts
@@ -1,7 +1,8 @@
 import type { RequestHandler } from "express";
 
 const sayWelcome: RequestHandler = (req, res) => {
-  res.send("Welcome to Wild Series");
+  console.info(req.query);
+  res.send(`"Welcome to Wild Series", ${req.query.name} !`);
 };
 
 export default { sayWelcome };
--- server/src/router.ts
+++ server/src/router.ts
@@ -24,5 +24,13 @@ router.get("/", sayActions.sayWelcome);
 
 import programActions from "./modules/program/programActions";
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+/* ************************************************************************* */
+//Declaration of a "categories" route
+
+import categoryActions from "./modules/category/categoryActions";
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 export default router;
